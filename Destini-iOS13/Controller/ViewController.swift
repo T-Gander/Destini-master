f96a1b5309433332990b063214eceaf4a3c342c5
Thomas Mac <thomas.gander99@gmail.com>
2023-11-12 23:38:25 -0700

    Finished project. Added a more scalable version of the story struct.

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -10,13 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var storyBrain = StoryBrain()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        btnChoice1.titleLabel?.adjustsFontSizeToFitWidth = true
+        btnChoice2.titleLabel?.adjustsFontSizeToFitWidth = true
+        updateUi()
     }
 
-    @IBAction func SelectedChoice(_ sender: Any) {
+    @IBAction func SelectedChoice(_ sender: UIButton) {
+        storyBrain.decision(decision: sender.titleLabel!.text!)
+        updateUi()
     }
     
+    func updateUi(){
+        lblQuestion.text = storyBrain.getCurrentStory()
+        btnChoice1.setTitle(storyBrain.getChoice1(), for: .normal)
+        btnChoice2.setTitle(storyBrain.getChoice2(), for: .normal)
+    }
+    
+    @IBOutlet weak var btnChoice1: UIButton!
+    @IBOutlet weak var btnChoice2: UIButton!
+    @IBOutlet weak var lblQuestion: UILabel!
 }
 
